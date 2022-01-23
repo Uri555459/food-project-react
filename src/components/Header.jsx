d3868437a8eaf0a6f0e1922b50f7b4e5884951fb
Uri <uri555459@gmail.com>
2022-01-23 12:37:27 +0300

    Fixed link header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
-  return <nav className="green darken-1">
-    <div className="nav-wrapper">
-      <Link to='/' className="brand-logo">React Shop</Link>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to='/contact'>Contact</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul>
-    </div>
-  </nav>
+  return (
+    <nav className="green darken-1">
+      <div className="nav-wrapper">
+        <Link to="/food-project-react" className="brand-logo">
+          Food Shop
+        </Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export {Header}
+export { Header };
