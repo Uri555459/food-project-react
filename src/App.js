cfbbeb5209af8d1daa2690bf3138664701331764
Uri <uri555459@gmail.com>
2022-01-23 12:31:22 +0300

    Added favicon

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
-import {Home} from "./pages/Home";
-import {About} from "./pages/About";
-import {Contact} from "./pages/Contact";
-import {NotFound} from "./pages/NotFound";
-import {Category} from "./pages/Category";
-import {Recipe} from "./pages/Recipe";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { NotFound } from "./pages/NotFound";
+import { Category } from "./pages/Category";
+import { Recipe } from "./pages/Recipe";
 
 
 function App() {
   return (
     <>
       <Router>
-        <Header/>
+        <Header />
         <main className='container content'>
           <Switch>
-            <Route path='/' component={Home} exact/>
-            <Route path='/about' component={About}/>
-            <Route path='/contact' component={Contact}/>
-            <Route path='/category/:name' component={Category}/>
-            <Route path='/meal/:id' component={Recipe}/>
-            <Route component={NotFound}/>
+            <Route path='/ood-project-react' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/category/:name' component={Category} />
+            <Route path='/meal/:id' component={Recipe} />
+            <Route component={NotFound} />
           </Switch>
         </main>
-        <Footer/>
+        <Footer />
       </Router>
     </>
   )
